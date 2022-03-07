76e2ba92acf069cc8751e605d9c49f59c838e46a
Thomas Moore <tmoorecode@gmail.com>
2022-03-07 15:50:14 -0500

    added component related projects

---
+++ React/ClassComponents/my-react-project/src/components/MyNewComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div>
+                We are in MyNewComponent!
+            </div>
+        )
+    }
+}
+
+
+export default MyNewComponent;
