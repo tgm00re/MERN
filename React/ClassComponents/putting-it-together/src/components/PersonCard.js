a0c1f81ffc48666d65cad45e4308613b44f84c80
Thomas Moore <tmoorecode@gmail.com>
2022-03-07 17:08:22 -0500

    added putting-it-together project

---
+++ React/ClassComponents/putting-it-together/src/components/PersonCard.js
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import React, {Component} from "react";
+
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    birthday = () => {
+        this.setState({ age: this.state.age + 1 });
+    }
+
+
+    render(){
+        const {firstName, lastName, hairColor} = this.props;
+        return(
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={this.birthday}>Happy Birthday!</button>
+            </div>
+        )
+    }
+}
+
+export default PersonCard
